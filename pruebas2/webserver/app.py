36977138c3e300632cd1316e6fad1d3810383806
jammes3 <jammesduran3@gmail.com>
2022-06-14 20:03:47 -0400

    web python led

--- pruebas2/webserver/app.py
+++ pruebas2/webserver/app.py
@@ -37,7 +37,7 @@ def index():
         }
 	return render_template('index.html', **templateData)
 	
-@app.route("/<deviceName>/<action>")
+@app.route("/")
 def action(deviceName, action):
 	if deviceName == 'ledRed':
 		actuator = ledRed
