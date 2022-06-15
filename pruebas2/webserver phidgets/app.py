51a34be79e496caf09c855efda769ba64eff0cbd
jammes3 <jammesduran3@gmail.com>
2022-06-15 00:02:34 -0400

    prueba

--- pruebas2/webserver phidgets/app.py
+++ pruebas2/webserver phidgets/app.py
@@ -64,9 +64,19 @@ def action(deviceName, action):
 
 @app.route("/archivocreado")
 def indeqx():
-	            #imprime el archivo
-	return render_template('archivocreado')
+	import os
+	archivo = open("/sys/class/thermal/thermal_zone0/temp")
+	a =(archivo.read())
+	b=str(a[0:2])
+	print(b)
+	archivo = open("./teplates/archivocreado.txt", "w")      #crea archivo
+	archivo.write(b)              #esribe el archivo
+	archivo.close()                                 #cierra el archivo
 
+	archivo = open("./teplates/archivocreado.txt")          #abre el archivo
+	a = (archivo.read())                           #lee el archivo
+	print(a)                                        #imprime el archivo
+	return render_template('archivocreado')
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
