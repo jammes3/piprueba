0b106e0b05a218cb335b981e653270a5ec7d807c
jammes3 <jammesduran3@gmail.com>
2022-06-14 23:05:11 -0400

    pruebas

--- pruebas2/webserver phidgets/sensor.py
+++ pruebas2/webserver phidgets/sensor.py
@@ -3,9 +3,9 @@ archivo = open("/sys/class/thermal/thermal_zone0/temp")
 a =(archivo.read())
 b= a[0:2]
 
-archivo = open("./archivocreado.txt", "w") 
+archivo = open("./archivocreado.csv", "w") 
 archivo.write(b)   
 
-archivo = open("./archivocreado.txt")          #abre el archivo
+archivo = open("./archivocreado.csv")          #abre el archivo
 c = (archivo.read())                           #lee el archivo
 print(c)                                        #imprime el archivo
